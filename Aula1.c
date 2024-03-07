a7bf723e03595889eedb97da372abdce7daa2a64
linveronez <162514074+linveronez@users.noreply.github.com>
2024-03-06 21:50:30 -0300

    Add files via upload

--- Aula1.c
+++ Aula1.c
@@ -17,7 +17,7 @@ int main() {
 	
 	/*Exemplo 2*/
 	
-	int Valor1, Valor2, total;
+	int Valor1, Valor2, Valor3, total, media;
 
 	
 	
@@ -27,26 +27,34 @@ int main() {
 	printf("Digite o segundo valor: ");
 	scanf("%d", &Valor2);
 	
-	printf("Os valores digitados sao: %d , %d \n" , Valor1 , Valor2);
+	printf("Digite o terceiro valor: ");
+	scanf("%d", &Valor3);
+	
+	printf("Os valores digitados sao: %d , %d , %d \n" , Valor1 , Valor2, Valor3);
 	
 	printf("O primeiro valor digitado e: %d \n", Valor1);
 	printf("O segundo valor digitado e: %d \n", Valor2);
-	
-	total = Valor1 + Valor2;
+	printf("O terceiro valor digitado e: %d \n", Valor3);
+		
+	total = Valor1 + Valor2 + Valor3;
 	
 	printf("A soma dos valores e: %d \n", total );
 	
-	total = Valor1 - Valor2;
+	total = Valor1 - Valor2 - Valor3;
 	
 	printf("A subtracao dos valores e: %d \n", total );
 	
-	total = Valor1 * Valor2;
+	total = Valor1 * Valor2 * Valor3;
 	
 	printf("A multiplicacao dos valores e: %d \n", total );
 	
-	total = Valor1 / Valor2;
+	total = (Valor1 / Valor2) / Valor3;
 	
 	printf("A divisao dos valores e: %d \n", total );
+	
+	media = (Valor1 + Valor2+ Valor3) / 3;
+	
+	printf("A media dos tres valores e: %d \n", media );
 	
 	/*imprime hello word para evitar a maldicao*/
 	printf("HELLO WORD!!!");
